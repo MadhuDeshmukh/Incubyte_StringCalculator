8543651a23867c1e801f980b7245d978bcdae27f
MadhuDeshmukh <madhudeshmukh219@gmail.com>
2021-10-17 01:14:56 +0530

    string_with_negative_numbers_should_throw_exception() method added, test case passed

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -7,22 +7,28 @@ import java.util.regex.Pattern;
 
 class StringCalculator {
 
-	public int add(String input) {
+	public int add(String input) throws Exception {
 
 		String[] numbers = input.split(",|\n");
 
 		if(input.isEmpty()) {
-			
+
 			return 0;
 		}else if(input.length() == 1){
-			
-			return addNum(numbers);
-			
+
+			if(Integer.parseInt(input) < 0) {
+				throw new RuntimeException("negatives not allowed " + Integer.parseInt(input) );
+				
+			}else
+			{
+				return Integer.parseInt(input);
+			}
+
 		}else if(input.startsWith("//")){
-			
+
 			String[] num = getDelimiterSeperatedNumbers(input);
 			return addNum(num);
-			
+
 		}else {
 
 			return addNum(numbers);
@@ -30,11 +36,12 @@ class StringCalculator {
 
 	}
 
-	
+
 
 	//Method for adding any amount of numbers
-	private int addNum(String [] integers) {
+	private int addNum(String [] integers) throws Exception{
 		int sum=0;
+
 
 		for(int i=0; i < integers.length; i++) {
 			sum =sum + Integer.parseInt(integers[i]);
@@ -43,17 +50,20 @@ class StringCalculator {
 
 	}
 
+	
+
+
 	//Method to get different delimiter separated numbers
 	private String[] getDelimiterSeperatedNumbers(String input) {
 		Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-		
+
 		if(matcher.matches()) {
-			
+
 			String delimiter = matcher.group(1);
 			String toSplit = matcher.group(2);
-			
+
 			return toSplit.split(delimiter);
-			
+
 		}
 		throw new RuntimeException("Wrong Custom Delimeter Format");
 	}
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -13,41 +13,42 @@ class StringCalculatorShould {
 
 
 	@Test
-	void empty_string_should_return_0() {
+	void empty_string_should_return_0() throws Exception {
 
 		assertEquals(0,stringCalculator.add(""));
 	}
 
 	@Test
-	void string_with_single_number_should_return_number_as_int() {
+	void string_with_single_number_should_return_number_as_int() throws Exception {
 
 		assertEquals(1, stringCalculator.add("1"));
 	}
 
 
 	@Test
-	void string_with_two_number_should_return_number_as_sum() {
+	void string_with_two_number_should_return_number_as_sum() throws Exception {
 		assertEquals(3, stringCalculator.add("1,2"));
 	}
-	
+
 	@Test
-	void string_with_any_numbers_should_return_number_as_sum() {
+	void string_with_any_numbers_should_return_number_as_sum() throws Exception {
 		assertEquals(21, stringCalculator.add("1,2,3,4,5,6"));
 	}
-	
+
 	@Test
-	void string_with_newLine_seperated_numbers_should_return_number_as_sum() {
+	void string_with_newLine_seperated_numbers_should_return_number_as_sum() throws Exception {
 		assertEquals(6, stringCalculator.add("1\n2,3"));
 	}
 
 	@Test
-	void string_with_different_delimeter_seperated_numbers_should_return_number_as_sum() {
+	void string_with_different_delimeter_seperated_numbers_should_return_number_as_sum() throws Exception {
 		assertEquals(3, stringCalculator.add("//;\n1;2"));
 	}
-	
+
 	@Test
-	void string_with_negative_numbers_should_return_exception() {
-		assertEquals(3, stringCalculator.add("-1,2"));
-	}
+	void string_with_negative_numbers_should_throw_exception() throws Exception{
+		
+		stringCalculator.add("-2");
+	}		
 
 }
